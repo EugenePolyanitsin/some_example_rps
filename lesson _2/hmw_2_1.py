83ecb87a95a04520bb839aefa4380673687b8c0a
EugenePolyanitsin <epolyanicin@yandex.ru>
2020-11-10 23:37:21 +0300

    [upd] second homework

--- lesson _2/hmw_2_1.py
+++ lesson _2/hmw_2_1.py
@@ -3,16 +3,6 @@
 # Использовать функцию type() для проверки типа.
 # Элементы списка можно не запрашивать у пользователя, а указать явно, в программе.
 
-# my_int = 5
-# my_float = 1.2
-# my_str = "Hello world"
-# my_list = ['a', '2']
-# my_tuple = ('b', '3')
-# my_dict = {'city': 'Moscow', 'country': 'Russia'}
-#
-# super_list = [my_int, my_float, my_str, my_list, my_tuple, my_dict]
-# for i in super_list:
-#     print(f'{i} is {type(i)}')
 t = ('e', '98', 'x')
 d = {'name': 'Eugene', 'year': '33'}
 my_list = ['one', 2, 1.18, True, -99, t, d]
