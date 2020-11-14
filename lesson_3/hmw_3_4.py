90ca055f14396c5b9f5c749d1eb96c6aecf8769a
EugenePolyanitsin <epolyanicin@yandex.ru>
2020-11-14 11:17:56 +0300

    third homework

---
+++ lesson_3/hmw_3_4.py
@@ -0,0 +1,14 @@
+def my_func(x, y):
+    return 1 / x ** abs(y)
+
+
+print(my_func(4, -4))
+
+
+def my_func_1(x, y):
+    for i in range(abs(y - 1)):
+        x *= x
+    return 1 / x
+
+
+print(my_func_1(2, -2))
