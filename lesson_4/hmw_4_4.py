858906e66ac9986ca24a80b9841f264b2850b040
EugenePolyanitsin <epolyanicin@yandex.ru>
2020-11-20 14:35:18 +0300

    four homework

---
+++ lesson_4/hmw_4_4.py
@@ -0,0 +1,5 @@
+from random import randint
+
+my_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+sec_list = [el for el in my_list if my_list.count(el) == 1]
+print(f"Исходный список\n{my_list}\nРезультат\n{sec_list}")
